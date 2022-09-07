e672492d7f524d5acc80467581b0a028ef29fb28
zachariah martin <zachariahsmartin@gmail.com>
2022-09-07 19:54:40 -0400

    commit tree 3df52482d85dcf2391b7bef52048a28cd1676b2e

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(){ 
+    return cats.push("Ralph")
+}
+
+function destructivelyPrependCat (){
+    return cats.unshift("Bob")
+}
+
+function destructivelyRemoveLastCat(){
+    return cats.pop()
+}
+
+function destructivelyRemoveFirstCat(){
+    return cats.shift()
+}
+
+function appendCat(){
+    const cats1 = [...cats, "Broom"]
+    return cats1
+}
+
+function prependCat(){
+    const cats2 = ["Arnold", ...cats]
+    return cats2
+}
+
+function removeLastCat() {
+    return cats.slice(0,-1)
+}
+
+function removeFirstCat(){
+    return cats.slice(1)
+}
